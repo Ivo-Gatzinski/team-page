5fcd2e935f6ec8f53108e594f096f590e91cb76f
Ezhko <ucsd.camp.21@gmail.com>
2021-10-15 17:18:56 -0700

    add consts

---
+++ src/html.js
@@ -0,0 +1,5 @@
+const fs = require(fs);
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
